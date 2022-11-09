8686d625d170dc937f88edb363d5752db6808a48
Matt McCormick <matt.mccormick@kitware.com>
2022-11-09 15:52:43 -0500

    feat: Add button to download the content link file

--- src/upload.js
+++ src/upload.js
@@ -1,19 +1,23 @@
 import { loadDefaultIdentity } from '@w3ui/keyring-core'
 import { uploadCarChunks, encodeFile, chunkBlocks, createUpload } from '@w3ui/uploader-core'
 
+import download  from './download.js'
+
 const SELECTORS = {
   uploadForm: '#upload-form',
   uploadFormTemplate: '#upload-form-template',
+  downloadForm: '#download-form',
   encodingTemplate: '#file-encoding-template',
   uploadingTemplate: '#file-uploading-template',
   uploadCompleteTemplate: '#upload-complete-template',
-  uploadErrorTemplate: '#upload-error-template'
+  uploadErrorTemplate: '#upload-errmr-template'
 }
 
 export class UploadFileForm extends window.HTMLElement {
   constructor () {
     super()
     this.form$ = document.querySelector(SELECTORS.uploadForm)
+    this.downloadForm$ = document.querySelector(SELECTORS.downloadForm)
     this.uploadFormTemplate$ = document.querySelector(SELECTORS.uploadFormTemplate)
     this.encodingTemplate$ = document.querySelector(SELECTORS.encodingTemplate)
     this.uploadingTemplate$ = document.querySelector(SELECTORS.uploadingTemplate)
@@ -22,9 +26,10 @@ export class UploadFileForm extends window.HTMLElement {
   }
 
   async connectedCallback () {
-    const templateContent = this.uploadFormTemplate$.content
+    const templateContent = this.uploadFormTemplate$.content.cloneNode(true)
     this.replaceChildren(templateContent)
     this.handleFileUpload = this.handleFileUpload.bind(this)
+    this.handleContentLinkDownload = this.handleContentLinkDownload.bind(this)
     this.form$ = document.querySelector(SELECTORS.uploadForm)
     this.form$.addEventListener('submit', this.handleFileUpload)
   }
@@ -57,23 +62,33 @@ export class UploadFileForm extends window.HTMLElement {
     }
   }
 
+  async handleContentLinkDownload (event) {
+    event.preventDefault()
+    download(`${this.file.name}.cid`, this.cid)
+
+    // Reset
+    await this.connectedCallback()
+  }
+
   toggleEncoding () {
-    const templateContent = this.encodingTemplate$.content
+    const templateContent = this.encodingTemplate$.content.cloneNode(true)
     this.replaceChildren(this.formatEncodingTemplateContent(templateContent))
   }
 
   toggleUploading () {
-    const templateContent = this.uploadingTemplate$.content
+    const templateContent = this.uploadingTemplate$.content.cloneNode(true)
     this.replaceChildren(this.formatUploadingTemplateContent(templateContent))
   }
 
   toggleUploadComplete () {
-    const templateContent = this.uploadCompleteTemplate$.content
+    const templateContent = this.uploadCompleteTemplate$.content.cloneNode(true)
     this.replaceChildren(this.formatUploadCompleteTemplateContent(templateContent))
+    this.downloadForm$ = document.querySelector(SELECTORS.downloadForm)
+    this.downloadForm$.addEventListener('submit', this.handleContentLinkDownload)
   }
 
   toggleUploadError () {
-    const templateContent = this.uploadErrorTemplate$.content
+    const templateContent = this.uploadErrorTemplate$.content.cloneNode(true)
     this.replaceChildren(this.formatUploadErrorTemplateContent(templateContent))
   }
 
@@ -102,11 +117,18 @@ export class UploadFileForm extends window.HTMLElement {
     slot.innerText = this.cid
     const hrefSlot = templateContent.querySelector('[data-root-cid-href-slot]')
     hrefSlot.href = `https://w3s.link/ipfs/${this.cid}`
+    hrefSlot.download = this.file.name
+    const fileNameSlot = templateContent.querySelector('[data-file-name-slot]')
+    fileNameSlot.innerText = this.file.name
     return templateContent
   }
 
   disconnectedCallback () {
     this.form$.removeEventListener('submit', this.handleFileUpload)
+    this.downloadForm$ = document.querySelector(SELECTORS.downloadForm)
+    if (this.downloadForm$) {
+      this.downloadForm$.removeEventListener('submit', this.handleContentLinkDownload)
+    }
   }
 }
 
